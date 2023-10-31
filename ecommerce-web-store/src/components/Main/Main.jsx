c35d7f73fabe1efec8ba4d28db0925ed5bdc17e7
Abir835 <abirhasan.raj.bd@gmail.com>
2023-10-31 18:47:01 +0600

    navber design

---
+++ ecommerce-web-store/src/components/Main/Main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navber from "../Navber/Navber";
+
+const Main = () =>{
+    return(
+        <div>
+            <Navber/>
+        </div>
+    );
+};
+
+
+export default Main;
